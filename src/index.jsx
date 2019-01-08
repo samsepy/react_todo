6b7c9f2869ec781ca850caf0c4a95f59d2175c8c
Hajime Todo <samsepylot@gmail.com>
2019-01-08 18:53:20 +0900

    TodoListコンポーネントの追加

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+class TodoList extends React.Component {
+  render() {
+    const todos = this.props.todos
+    return (
+      <div>
+        {todos.map(todo => (
+          <div key={todo.id}>{todo.content}</div>
+        ))}
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          content: "todo1"
+        },
+        {
+          id: 2,
+          content: "todo2"
+        },
+        {
+          id: 3,
+          content: "todo3"
+        },
+      ]
+    }
+
+  }
   render () {
-    return <p>React TODO</p>;
+    return (
+      <TodoList todos={this.state.todos}/>
+    );
   }
 }
 
